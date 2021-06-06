656da88fe6f3f0abbfccf7cc3cabd1f68c660d0d
huutq-88 <84846147+huutq-88@users.noreply.github.com>
2021-06-06 11:07:42 +0700

    sx

--- CtyTinLuong/QuanLySanXuat/Class/clsPhieu_tbPhieu - Copy.cs
+++ CtyTinLuong/QuanLySanXuat/Class/clsPhieu_tbPhieu - Copy.cs
@@ -14,7 +14,41 @@ namespace  CtyTinLuong
 	/// Purpose: Data Access class for the table 'Phieu_tbPhieu'.
 	/// </summary>
 	public partial class clsPhieu_tbPhieu : clsDBInteractionBase
-    {//pr_Phieu_tbPhieu_Update_BienTrangThai
+    {//pr_Phieu_tbPhieu_SelectAll_Load_DaTa_Day
+        public DataTable SelectAll_Load_DaTa_Day(int sotrang, DateTime ngay_batdau, DateTime ngay_ketthuc, string ma_phieu)
+        {
+            SqlCommand scmCmdToExecute = new SqlCommand();
+            scmCmdToExecute.CommandText = "dbo.[pr_Phieu_tbPhieu_SelectAll_Load_DaTa_Day]";
+            scmCmdToExecute.CommandType = CommandType.StoredProcedure;
+            DataTable dtToReturn = new DataTable("pr_Phieu_tbPhieu_SelectAll_Load_DaTa_Day");
+            SqlDataAdapter sdaAdapter = new SqlDataAdapter(scmCmdToExecute);
+
+            // Use base class' connection object
+            scmCmdToExecute.Connection = m_scoMainConnection;
+
+            try
+            {
+                m_scoMainConnection.Open();
+                scmCmdToExecute.Parameters.Add(new SqlParameter("@SoTrang", SqlDbType.Int, 4, ParameterDirection.Input, false, 10, 0, "", DataRowVersion.Proposed, sotrang));
+                scmCmdToExecute.Parameters.Add(new SqlParameter("@ngay_batdau", SqlDbType.SmallDateTime, 3, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Proposed, ngay_batdau));
+                scmCmdToExecute.Parameters.Add(new SqlParameter("@ngay_ketthuc", SqlDbType.SmallDateTime, 3, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Proposed, ngay_ketthuc));
+                scmCmdToExecute.Parameters.Add(new SqlParameter("@ma_phieu", SqlDbType.NVarChar, 50, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Proposed, ma_phieu));
+                sdaAdapter.Fill(dtToReturn);
+                return dtToReturn;
+            }
+            catch (Exception ex)
+            {
+                // some error occured. Bubble it to caller and encapsulate Exception object
+                throw new Exception("pr_Phieu_tbPhieu_SelectAll_Load_DaTa_Day", ex);
+            }
+            finally
+            {
+                //Close connection.
+                m_scoMainConnection.Close();
+                scmCmdToExecute.Dispose();
+                sdaAdapter.Dispose();
+            }
+        }
         public void Update_BienTrangThai()
         {
 
