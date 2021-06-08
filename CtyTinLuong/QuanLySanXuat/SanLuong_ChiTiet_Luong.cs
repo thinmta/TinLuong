4f60ddd3bc11303c9215261bbb2ed3750c8c1f95
huutq-88 <84846147+huutq-88@users.noreply.github.com>
2021-06-08 22:46:38 +0700

    a

---
+++ CtyTinLuong/QuanLySanXuat/SanLuong_ChiTiet_Luong.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CtyTinLuong
+{
+    public partial class SanLuong_ChiTiet_Luong : Form
+    {
+        public SanLuong_ChiTiet_Luong()
+        {
+            InitializeComponent();
+        }
+    }
+}
