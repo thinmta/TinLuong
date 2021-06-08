cad10138f066b3b71074218bc51e503e0262017d
trieunv <ngvtrieusqtt@gmail.com>
2021-06-08 09:35:22 +0700

    in

--- CtyTinLuong/Luong_ChamCong/T_PrintChamCongToGapDan.cs
+++ CtyTinLuong/Luong_ChamCong/T_PrintChamCongToGapDan.cs
@@ -3,20 +3,86 @@ using System.Drawing;
 using System.Collections;
 using System.ComponentModel;
 using DevExpress.XtraReports.UI;
+using System.Collections.Generic;
 
 namespace CtyTinLuong.Luong_ChamCong
 {
     public partial class T_PrintChamCongToGapDan : DevExpress.XtraReports.UI.XtraReport
     {
+        List<XRTableCell> Ds_Ngay;
+        List<XRTableCell> Ds_Ngay_Header;
         public T_PrintChamCongToGapDan()
         {
             InitializeComponent();
+            Ds_Ngay.Add(Ngay1);
+            Ds_Ngay.Add(Ngay2);
+            Ds_Ngay.Add(Ngay3);
+            Ds_Ngay.Add(Ngay4);
+            Ds_Ngay.Add(Ngay5);
+            Ds_Ngay.Add(Ngay6);
+            Ds_Ngay.Add(Ngay7);
+            Ds_Ngay.Add(Ngay8);
+            Ds_Ngay.Add(Ngay9);
+            Ds_Ngay.Add(Ngay10);
+            Ds_Ngay.Add(Ngay11);
+            Ds_Ngay.Add(Ngay12);
+            Ds_Ngay.Add(Ngay13);
+            Ds_Ngay.Add(Ngay14);
+            Ds_Ngay.Add(Ngay15);
+            Ds_Ngay.Add(Ngay16);
+            Ds_Ngay.Add(Ngay17);
+            Ds_Ngay.Add(Ngay18);
+            Ds_Ngay.Add(Ngay19);
+            Ds_Ngay.Add(Ngay20);
+            Ds_Ngay.Add(Ngay21);
+            Ds_Ngay.Add(Ngay22);
+            Ds_Ngay.Add(Ngay23);
+            Ds_Ngay.Add(Ngay24);
+            Ds_Ngay.Add(Ngay25);
+            Ds_Ngay.Add(Ngay26);
+            Ds_Ngay.Add(Ngay27);
+            Ds_Ngay.Add(Ngay28);
+            Ds_Ngay.Add(Ngay29);
+            Ds_Ngay.Add(Ngay30);
+            Ds_Ngay.Add(Ngay31);
+            //
+            Ds_Ngay_Header.Add(ng1);
+            Ds_Ngay_Header.Add(ng2);
+            Ds_Ngay_Header.Add(ng3);
+            Ds_Ngay_Header.Add(ng4);
+            Ds_Ngay_Header.Add(ng5);
+            Ds_Ngay_Header.Add(ng6);
+            Ds_Ngay_Header.Add(ng7);
+            Ds_Ngay_Header.Add(ng8);
+            Ds_Ngay_Header.Add(ng9);
+            Ds_Ngay_Header.Add(ng10);
+            Ds_Ngay_Header.Add(ng11);
+            Ds_Ngay_Header.Add(ng12);
+            Ds_Ngay_Header.Add(ng13);
+            Ds_Ngay_Header.Add(ng14);
+            Ds_Ngay_Header.Add(ng15);
+            Ds_Ngay_Header.Add(ng16);
+            Ds_Ngay_Header.Add(ng17);
+            Ds_Ngay_Header.Add(ng18);
+            Ds_Ngay_Header.Add(ng19);
+            Ds_Ngay_Header.Add(ng20);
+            Ds_Ngay_Header.Add(ng21);
+            Ds_Ngay_Header.Add(ng22);
+            Ds_Ngay_Header.Add(ng23);
+            Ds_Ngay_Header.Add(ng24);
+            Ds_Ngay_Header.Add(ng25);
+            Ds_Ngay_Header.Add(ng26);
+            Ds_Ngay_Header.Add(ng27);
+            Ds_Ngay_Header.Add(ng28);
+            Ds_Ngay_Header.Add(ng29);
+            Ds_Ngay_Header.Add(ng30);
+            Ds_Ngay_Header.Add(ng31);
 
             if (DateTime.Now.Month <= 9) pMonth.Value = "0" + DateTime.Now.Month.ToString();
             else pMonth.Value = DateTime.Now.Month.ToString();
             pYear.Value = DateTime.Now.Year;
             setThu();
-            setMauTableDetail();
+           // setMauTableDetail();
         }
 
         private void ReportHeader_BeforePrint(object sender, System.Drawing.Printing.PrintEventArgs e)
@@ -82,213 +148,240 @@ namespace CtyTinLuong.Luong_ChamCong
         public void setThu()
         {
             DateTime dtnow = DateTime.Now;
-            DateTime date_ = new DateTime(dtnow.Year, dtnow.Month, 1);
+            // 
             int ngaycuathang_ = (((new DateTime(dtnow.Year, dtnow.Month, 1)).AddMonths(1)).AddDays(-1)).Day;
-            string thu_ = LayThu(date_);
 
-            ng1.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (1)));
-            if (ng1.Text.Contains("CN"))
+            for (int i = 0; i < ngaycuathang_; ++i)
             {
-                ng1.BackColor = Color.LightGray;
+                Ds_Ngay_Header[i].Text = (i + 1) + "\n" + LayThu(new DateTime(dtnow.Year, dtnow.Month, (i + 1)));
+                if (Ds_Ngay_Header[i].Text.Contains("CN"))
+                {
+                    Ds_Ngay_Header[i].BackColor = Color.LightGray;
+                    Ds_Ngay_Header[i].BackColor = Color.LightGray;
+                    Ds_Ngay_Header[i].ForeColor = Color.Red;
+                    Ds_Ngay_Header[i].ForeColor = Color.Red;
+                }
             }
-
-            ng2.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (2)));
-            if (ng2.Text.Contains("CN"))
+            //
+            for (int i = 0; i < ngaycuathang_; ++i)
             {
-                ng2.BackColor = Color.LightGray;
+                Ds_Ngay[i].Text = (i + 1) + "\n" + LayThu(new DateTime(dtnow.Year, dtnow.Month, (i + 1)));
+                if (Ds_Ngay[i].Text.Contains("CN"))
+                {
+                    Ds_Ngay[i].BackColor = Color.LightGray;
+                    Ds_Ngay[i].BackColor = Color.LightGray;
+                    Ds_Ngay[i].ForeColor = Color.Red;
+                    Ds_Ngay[i].ForeColor = Color.Red;
+                }
             }
+            ////
+            //DateTime date_ = new DateTime(dtnow.Year, dtnow.Month, 1);
+            //int ngaycuathang_ = (((new DateTime(dtnow.Year, dtnow.Month, 1)).AddMonths(1)).AddDays(-1)).Day;
+            //string thu_ = LayThu(date_);
 
-            ng3.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (3)));
-            if (ng3.Text.Contains("CN"))
-            {
-                ng3.BackColor = Color.LightGray;
-            }
+            //ng1.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (1)));
+            //if (ng1.Text.Contains("CN"))
+            //{
+            //    ng1.BackColor = Color.LightGray;
+            //}
 
+            //ng2.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (2)));
+            //if (ng2.Text.Contains("CN"))
+            //{
+            //    ng2.BackColor = Color.LightGray;
+            //}
 
-            ng4.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (4)));
-            if (ng4.Text.Contains("CN"))
-            {
-                ng4.BackColor = Color.LightGray;
-            }
-
-
-            ng5.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (5)));
-            if (ng5.Text.Contains("CN"))
-            {
-                ng5.BackColor = Color.LightGray;
-            }
-
-
-            ng6.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (6)));
-            if (ng6.Text.Contains("CN"))
-            {
-                ng6.BackColor = Color.LightGray;
-            }
+            //ng3.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (3)));
+            //if (ng3.Text.Contains("CN"))
+            //{
+            //    ng3.BackColor = Color.LightGray;
+            //}
 
 
-            ng7.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (7)));
-            if (ng7.Text.Contains("CN"))
-            {
-                ng7.BackColor = Color.LightGray;
-            }
+            //ng4.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (4)));
+            //if (ng4.Text.Contains("CN"))
+            //{
+            //    ng4.BackColor = Color.LightGray;
+            //}
 
 
-            ng8.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (8)));
-            if (ng8.Text.Contains("CN"))
-            {
-                ng8.BackColor = Color.LightGray;
-            }
+            //ng5.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (5)));
+            //if (ng5.Text.Contains("CN"))
+            //{
+            //    ng5.BackColor = Color.LightGray;
+            //}
 
 
-            ng9.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (9)));
-            if (ng9.Text.Contains("CN"))
-            {
-                ng9.BackColor = Color.LightGray;
-            }
-
-            ng10.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (10)));
-            if (ng10.Text.Contains("CN"))
-            {
-                ng10.BackColor = Color.LightGray;
-            }
-
-            ng11.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (11)));
-            if (ng11.Text.Contains("CN"))
-            {
-                ng11.BackColor = Color.LightGray;
-            }
-
-            ng12.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (12)));
-            if (ng12.Text.Contains("CN"))
-            {
-                ng12.BackColor = Color.LightGray;
-            }
-
-            ng13.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (13)));
-            if (ng13.Text.Contains("CN"))
-            {
-                ng13.BackColor = Color.LightGray;
-            }
+            //ng6.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (6)));
+            //if (ng6.Text.Contains("CN"))
+            //{
+            //    ng6.BackColor = Color.LightGray;
+            //}
 
 
-            ng14.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (14)));
-            if (ng14.Text.Contains("CN"))
-            {
-                ng14.BackColor = Color.LightGray;
-            }
+            //ng7.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (7)));
+            //if (ng7.Text.Contains("CN"))
+            //{
+            //    ng7.BackColor = Color.LightGray;
+            //}
 
 
-            ng15.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (15)));
-            if (ng15.Text.Contains("CN"))
-            {
-                ng15.BackColor = Color.LightGray;
-            }
+            //ng8.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (8)));
+            //if (ng8.Text.Contains("CN"))
+            //{
+            //    ng8.BackColor = Color.LightGray;
+            //}
 
 
-            ng16.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (16)));
-            if (ng16.Text.Contains("CN"))
-            {
-                ng16.BackColor = Color.LightGray;
-            }
+            //ng9.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (9)));
+            //if (ng9.Text.Contains("CN"))
+            //{
+            //    ng9.BackColor = Color.LightGray;
+            //}
+
+            //ng10.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (10)));
+            //if (ng10.Text.Contains("CN"))
+            //{
+            //    ng10.BackColor = Color.LightGray;
+            //}
+
+            //ng11.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (11)));
+            //if (ng11.Text.Contains("CN"))
+            //{
+            //    ng11.BackColor = Color.LightGray;
+            //}
+
+            //ng12.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (12)));
+            //if (ng12.Text.Contains("CN"))
+            //{
+            //    ng12.BackColor = Color.LightGray;
+            //}
+
+            //ng13.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (13)));
+            //if (ng13.Text.Contains("CN"))
+            //{
+            //    ng13.BackColor = Color.LightGray;
+            //}
 
 
-            ng17.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (17)));
-            if (ng17.Text.Contains("CN"))
-            {
-                ng17.BackColor = Color.LightGray;
-            }
+            //ng14.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (14)));
+            //if (ng14.Text.Contains("CN"))
+            //{
+            //    ng14.BackColor = Color.LightGray;
+            //}
 
 
-            ng18.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (18)));
-            if (ng18.Text.Contains("CN"))
-            {
-                ng18.BackColor = Color.LightGray;
-            }
+            //ng15.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (15)));
+            //if (ng15.Text.Contains("CN"))
+            //{
+            //    ng15.BackColor = Color.LightGray;
+            //}
 
 
-            ng19.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (19)));
-            if (ng19.Text.Contains("CN"))
-            {
-                ng19.BackColor = Color.LightGray;
-            }
-
-            ng20.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (20)));
-            if (ng20.Text.Contains("CN"))
-            {
-                ng20.BackColor = Color.LightGray;
-            }
+            //ng16.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (16)));
+            //if (ng16.Text.Contains("CN"))
+            //{
+            //    ng16.BackColor = Color.LightGray;
+            //}
 
 
-            ng21.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (21)));
-            if (ng21.Text.Contains("CN"))
-            {
-                ng21.BackColor = Color.LightGray;
-            }
-
-            ng22.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (22)));
-            if (ng22.Text.Contains("CN"))
-            {
-                ng22.BackColor = Color.LightGray;
-            }
-
-            ng23.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (23)));
-            if (ng23.Text.Contains("CN"))
-            {
-                ng23.BackColor = Color.LightGray;
-            }
+            //ng17.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (17)));
+            //if (ng17.Text.Contains("CN"))
+            //{
+            //    ng17.BackColor = Color.LightGray;
+            //}
 
 
-            ng24.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (24)));
-            if (ng24.Text.Contains("CN"))
-            {
-                ng24.BackColor = Color.LightGray;
-            }
+            //ng18.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (18)));
+            //if (ng18.Text.Contains("CN"))
+            //{
+            //    ng18.BackColor = Color.LightGray;
+            //}
 
 
-            ng25.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (25)));
-            if (ng25.Text.Contains("CN"))
-            {
-                ng25.BackColor = Color.LightGray;
-            }
+            //ng19.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (19)));
+            //if (ng19.Text.Contains("CN"))
+            //{
+            //    ng19.BackColor = Color.LightGray;
+            //}
+
+            //ng20.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (20)));
+            //if (ng20.Text.Contains("CN"))
+            //{
+            //    ng20.BackColor = Color.LightGray;
+            //}
 
 
-            ng26.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (26)));
-            if (ng26.Text.Contains("CN"))
-            {
-                ng26.BackColor = Color.LightGray;
-            }
+            //ng21.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (21)));
+            //if (ng21.Text.Contains("CN"))
+            //{
+            //    ng21.BackColor = Color.LightGray;
+            //}
+
+            //ng22.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (22)));
+            //if (ng22.Text.Contains("CN"))
+            //{
+            //    ng22.BackColor = Color.LightGray;
+            //}
+
+            //ng23.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (23)));
+            //if (ng23.Text.Contains("CN"))
+            //{
+            //    ng23.BackColor = Color.LightGray;
+            //}
 
 
-            ng27.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (27)));
-            if (ng27.Text.Contains("CN"))
-            {
-                ng27.BackColor = Color.LightGray;
-            }
+            //ng24.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (24)));
+            //if (ng24.Text.Contains("CN"))
+            //{
+            //    ng24.BackColor = Color.LightGray;
+            //}
 
 
-            ng28.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (28)));
-            if (ng28.Text.Contains("CN"))
-            {
-                ng28.BackColor = Color.LightGray;
-            }
+            //ng25.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (25)));
+            //if (ng25.Text.Contains("CN"))
+            //{
+            //    ng25.BackColor = Color.LightGray;
+            //}
 
 
-            ng29.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (29)));
-            if (ng29.Text.Contains("CN"))
-            {
-                ng29.BackColor = Color.LightGray;
-            }
+            //ng26.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (26)));
+            //if (ng26.Text.Contains("CN"))
+            //{
+            //    ng26.BackColor = Color.LightGray;
+            //}
 
-            ng30.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (30)));
-            if (ng30.Text.Contains("CN"))
-            {
-                ng30.BackColor = Color.LightGray;
-            }
+
+            //ng27.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (27)));
+            //if (ng27.Text.Contains("CN"))
+            //{
+            //    ng27.BackColor = Color.LightGray;
+            //}
+
+
+            //ng28.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (28)));
+            //if (ng28.Text.Contains("CN"))
+            //{
+            //    ng28.BackColor = Color.LightGray;
+            //}
+
+
+            //ng29.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (29)));
+            //if (ng29.Text.Contains("CN"))
+            //{
+            //    ng29.BackColor = Color.LightGray;
+            //}
+
+            //ng30.Text = LayThu(new DateTime(dtnow.Year, dtnow.Month, (30)));
+            //if (ng30.Text.Contains("CN"))
+            //{
+            //    ng30.BackColor = Color.LightGray;
+            //}
 
         }
 
         //set mau bang du lieu:
-        public void setMauTableDetail()
+        public void setMauTableDetail2()
         {
             DateTime dtnow = DateTime.Now;
             DateTime date_ = new DateTime(dtnow.Year, dtnow.Month, 1);
