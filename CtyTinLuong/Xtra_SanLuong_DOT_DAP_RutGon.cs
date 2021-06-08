a4aa29f010cb971c371bf43f4a234d9bbed0147a
huutq-88 <84846147+huutq-88@users.noreply.github.com>
2021-06-08 19:19:57 +0700

    a

---
+++ CtyTinLuong/Xtra_SanLuong_DOT_DAP_RutGon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace CtyTinLuong
+{
+    public partial class Xtra_SanLuong_DOT_DAP_RutGon : DevExpress.XtraReports.UI.XtraReport
+    {
+        public Xtra_SanLuong_DOT_DAP_RutGon()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
